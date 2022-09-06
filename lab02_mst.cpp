19522c200ba9bd19ed27c15d2d665f0f97015d40
danila <rezortiez@mail.ru>
2022-09-06 23:06:53 +0300

    minor refactoring

--- lab02_mst.cpp
+++ lab02_mst.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
 #include "lab02_min_switch.h"
 
-#define HOUR 3600
-
 bool check(int h, int m) {return h > 23 || m > 59 ? false : true;}
 
-void print_info(int sec, int hour, int min)
+int print_info(int hour, int min)
 {
-    if (sec % HOUR == 0)
+    if (min == 0)
     {
-        if (sec == 0) printf("Полночь.\n");
-        else if (sec == HOUR) printf("Час ночи ровно.\n");
-        else if (sec == 12 * HOUR) printf("Полдень.\n");
-        else if (sec >= 2 * HOUR && sec <= 4 * HOUR) printf("%d часа ночи ровно.\n", sec / HOUR);
-        else if (sec >= 5 * HOUR && sec < 12 * HOUR) printf("%d часов утра ровно.\n", sec / HOUR);
-        else if (sec == 13 * HOUR) printf("Час дня ровно.\n");
-        else if (sec > 13 * HOUR && sec <= 17 * HOUR) printf("%d часа дня ровно.\n", (sec / HOUR) - 12);
-        else if (sec >= 18 * HOUR && sec <= 23 * HOUR) printf("%d часов вечера ровно.\n", (sec / HOUR) - 12);
+        if (hour == 0) printf("Полночь.\n");
+        else if (hour == 1) printf("Час ночи ровно.\n");
+        else if (hour == 12) printf("Полдень.\n");
+        else if (hour >= 2 && hour <= 4 ) printf("%d часа ночи ровно.\n", hour);
+        else if (hour >= 5 && hour < 12) printf("%d часов утра ровно.\n", hour);
+        else if (hour == 13) printf("Час дня ровно.\n");
+        else if (hour > 13 && hour <= 17) printf("%d часа дня ровно.\n", hour - 12);
+        else if (hour >= 18 && hour <= 23) printf("%d часов вечера ровно.\n", hour - 12);
     } else {
-        if (sec / HOUR  == 1 ) night_switch1(hour, min);
-        else if (sec / HOUR > 1 && sec / HOUR < 5) night_switch(hour, min);
-        else if (sec / HOUR >= 5 && sec / HOUR < 12) morning_switch(hour, min);
-        else if (sec / HOUR == 12) midday_switch(hour, min);
-        else if (sec / HOUR == 13) afternoon_switch1(hour - 12, min);
-        else if (sec / HOUR > 13 && sec / HOUR < 17) afternoon_switch(hour - 12, min);
-        else if (sec / HOUR == 17) afternoon_switch_5(hour - 12, min);
-        else if (sec / HOUR >= 18 && sec / HOUR <= 23) evening_switch(hour - 12, min);
+        if (hour  == 1 ) night_switch1(hour, min);
+        else if (hour > 1 && hour < 5) night_switch(hour, min);
+        else if (hour >= 5 && hour < 12) morning_switch(hour, min);
+        else if (hour == 12) midday_switch(hour, min);
+        else if (hour == 13) afternoon_switch1(hour - 12, min);
+        else if (hour > 13 && hour < 17) afternoon_switch(hour - 12, min);
+        else if (hour == 17) afternoon_switch_5(hour - 12, min);
+        else if (hour >= 18 && hour <= 23) evening_switch(hour - 12, min);
     }
+
+    return 0;
 }
 
 int main()
 {
     unsigned int get_h, get_m;
     scanf("%d %d", &get_h, &get_m);
-    unsigned int totalsec = 0;
-    
-    if (check(get_h, get_m))
-    {
-        totalsec = get_h * 3600 + get_m * 60;
-        print_info(totalsec, get_h, get_m);
-    } else {
-        printf("incorrect input");
-    }
-
+    check(get_h, get_m) ? print_info(get_h, get_m) : printf("incorrect input");
     return 0;
 }
